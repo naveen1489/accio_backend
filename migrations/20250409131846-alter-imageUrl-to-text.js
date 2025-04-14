5c2d35ea80300eeec4583d74c9ec2e3d0de4ade6
unknown <naveengupta1489@gmail.com>
2025-04-14 20:46:48 +0530

    Corrected image migration

--- migrations/20250409131846-alter-imageUrl-to-text.js
+++ migrations/20250409131846-alter-imageUrl-to-text.js
@@ -1,26 +1,14 @@
 'use strict';
-
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
-        // Set default value for existing NULL entries
-        await queryInterface.sequelize.query(`
-            UPDATE "Restaurants"
-            SET "imageUrl" = ''
-            WHERE "imageUrl" IS NULL
-        `);
-
-        // Alter the column type to TEXT
-        await queryInterface.changeColumn('Restaurants', 'imageUrl', {
-            type: Sequelize.TEXT,
-            allowNull: false,
-        });
-    },
-
-    down: async (queryInterface, Sequelize) => {
-        // Revert the column type to STRING
-        await queryInterface.changeColumn('Restaurants', 'imageUrl', {
-            type: Sequelize.STRING,
-            allowNull: false,
-        });
-    },
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'imageUrl', {
+      type: Sequelize.TEXT,
+      allowNull: false,
+      defaultValue: '',
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'imageUrl');
+  },
 };
+ 
