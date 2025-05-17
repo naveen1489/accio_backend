6fd2a0ee1fdd6696c2362187207a5d2defc421e3
unknown <naveengupta1489@gmail.com>
2025-05-17 20:20:41 +0530

    added addressid in order and subscription

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Subscription, Order, Restaurant, Menu, User , Consumer} = require('../models');
+const { Subscription, Order, Restaurant, Menu, User , Consumer, Address} = require('../models');
 
 exports.createSubscription = async (req, res) => {
   try {
