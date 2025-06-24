499130a6a36292c9f7c76b502fe0135e97b6f30a
unknown <naveengupta1489@gmail.com>
2025-06-24 22:21:13 +0530

    get complint api

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -363,6 +363,25 @@ exports.getComplaintsByRestaurant = async (req, res) => {
       limit: parseInt(limit),
       offset: parseInt(offset),
       order: [['createdAt', 'DESC']],
+      include: [
+        {
+          model: Consumer,
+          as: 'consumer',
+          attributes: ['id', 'name'], // Include consumer name
+        },
+        {
+          model: Menu,
+          as: 'menu',
+          attributes: ['id', 'menuName'], // Include menu name
+          include: [
+            {
+              model: MenuItem,
+              as: 'menuItems',
+              attributes: ['id', 'itemName', 'itemCategory'], // Include menu items
+            },
+          ],
+        },
+      ],
     });
 
     res.status(200).json({
@@ -400,6 +419,25 @@ exports.getComplaintsByConsumer = async (req, res) => {
       limit: parseInt(limit),
       offset: parseInt(offset),
       order: [['createdAt', 'DESC']],
+      include: [
+        {
+          model: Consumer,
+          as: 'consumer',
+          attributes: ['id', 'name'], // Include consumer name
+        },
+        {
+          model: Menu,
+          as: 'menu',
+          attributes: ['id', 'menuName'], // Include menu name
+          include: [
+            {
+              model: MenuItem,
+              as: 'menuItems',
+              attributes: ['id', 'itemName', 'itemCategory'], // Include menu items
+            },
+          ],
+        },
+      ],
     });
 
     res.status(200).json({
--- models/adminMessage.js
+++ models/adminMessage.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
         primaryKey: true,
       },
       userId: {
-        type: DataTypes.UUIDV4,
+        type: DataTypes.UUID,
         allowNull: false,
       },
       message: {
